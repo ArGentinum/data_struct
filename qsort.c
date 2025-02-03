6852205eb45b23faf09e26d0ab67fe202ae81495
ArGentinum <123313917+ArGentinum@users.noreply.github.com>
2025-02-03 02:23:19 -0800

    Update qsort.c

--- qsort.c
+++ qsort.c
@@ -1,4 +1,3 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 void swap(int *x,int *y){
     int temp=*x;
@@ -7,27 +6,30 @@ void swap(int *x,int *y){
 }
 
 int qsort(int *a,int max){
-    int l,r,key=a[max-1],l_index=0,r_index=max-2;
+    int l,r,key=a[0],l_index=0,r_index=max-2;
     swap(&a[0],&a[max-1]);
     while(l_index<=r_index){
         while(key>a[l_index]){
-            ++l_index;
+            l_index++;
             printf("left index:%d\n",l_index);
         }
 
-        while(key<a[r_index]){
-            --r_index;
+        while(key<a[r_index]&&r_index>=0){
+            r_index--;
             printf("right index:%d\n",r_index);
         }
+        if(l_index<=r_index)
         swap(&a[l_index],&a[r_index]);
-        l_index=0;
-        r_index=max-2;
+        //l_index=0;
+        //r_index=max-2;
+        else
+        return l_index;
     }
     return l_index;
 }
 int main() {
     // Write C code here
-    int a[]={7,1,9,6,0,5},max=sizeof(a)/sizeof(a[0]),index;
+    int a[]={9,6,7,1,0,-1,2},max=sizeof(a)/sizeof(a[0]),index;
     index=qsort(a,max);
     printf("%d\n",index);
     return 0;
